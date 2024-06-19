b0051a54157c554301996e9531acfbe4a1185a0f
Nikolya <92371529+MrHiroko@users.noreply.github.com>
2024-06-19 14:19:19 +0700

    deleted:    Rozzcov/.gitignore 	deleted:    Rozzcov/.vscode/extensions.json 	deleted:    Rozzcov/README.md 	deleted:    Rozzcov/index.html 	deleted:    Rozzcov/jsconfig.json 	deleted:    Rozzcov/package.json 	deleted:    Rozzcov/public/favicon.ico 	new file:   Rozzcov/quasar-project/.editorconfig 	new file:   Rozzcov/quasar-project/.eslintignore 	new file:   Rozzcov/quasar-project/.eslintrc.cjs 	new file:   Rozzcov/quasar-project/.gitignore 	new file:   Rozzcov/quasar-project/.npmrc 	new file:   Rozzcov/quasar-project/.vscode/extensions.json 	new file:   Rozzcov/quasar-project/.vscode/settings.json 	new file:   Rozzcov/quasar-project/README.md 	new file:   Rozzcov/quasar-project/index.html 	new file:   Rozzcov/quasar-project/jsconfig.json 	new file:   Rozzcov/quasar-project/package-lock.json 	new file:   Rozzcov/quasar-project/package.json 	new file:   Rozzcov/quasar-project/postcss.config.cjs 	new file:   Rozzcov/quasar-project/public/favicon.ico 	new file:   Rozzcov/quasar-project/public/icons/favicon-128x128.png 	new file:   Rozzcov/quasar-project/public/icons/favicon-16x16.png 	new file:   Rozzcov/quasar-project/public/icons/favicon-32x32.png 	new file:   Rozzcov/quasar-project/public/icons/favicon-96x96.png 	new file:   Rozzcov/quasar-project/quasar.config.js 	new file:   Rozzcov/quasar-project/src/App.vue 	new file:   Rozzcov/quasar-project/src/assets/quasar-logo-vertical.svg 	new file:   Rozzcov/quasar-project/src/boot/.gitkeep 	new file:   Rozzcov/quasar-project/src/components/EssentialLink.vue 	new file:   Rozzcov/quasar-project/src/css/app.scss 	new file:   Rozzcov/quasar-project/src/css/quasar.variables.scss 	new file:   Rozzcov/quasar-project/src/layouts/MainLayout.vue 	new file:   Rozzcov/quasar-project/src/pages/ErrorNotFound.vue 	new file:   Rozzcov/quasar-project/src/pages/IndexPage.vue 	new file:   Rozzcov/quasar-project/src/router/index.js 	new file:   Rozzcov/quasar-project/src/router/routes.js 	deleted:    Rozzcov/src/App.vue 	deleted:    Rozzcov/src/assets/base.css 	deleted:    Rozzcov/src/assets/logo.svg 	deleted:    Rozzcov/src/assets/main.css 	deleted:    Rozzcov/src/components/HelloWorld.vue 	deleted:    Rozzcov/src/components/TheWelcome.vue 	deleted:    Rozzcov/src/components/icons/IconCommunity.vue 	deleted:    Rozzcov/src/components/icons/IconDocumentation.vue 	deleted:    Rozzcov/src/components/icons/IconEcosystem.vue 	deleted:    Rozzcov/src/components/icons/IconSupport.vue 	deleted:    Rozzcov/src/components/icons/IconTooling.vue 	deleted:    Rozzcov/src/main.js 	deleted:    Rozzcov/vite.config.js

---
+++ Rozzcov/quasar-project/src/App.vue
@@ -0,0 +1,22 @@
+
+<script>
+export default {
+  setup () {
+    return {
+      lorem: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 250px
+</style>
+
+<script setup>
+defineOptions({
+  name: 'App'
+});
+</script>
