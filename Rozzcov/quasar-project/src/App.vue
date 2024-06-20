8f85c24b9cf1a36ca8e5f528f23c68f635fb8316
Nikolya <92371529+MrHiroko@users.noreply.github.com>
2024-06-20 20:26:28 +0700

    modified:   Rozzcov/quasar-project/package-lock.json 	modified:   Rozzcov/quasar-project/package.json 	modified:   Rozzcov/quasar-project/quasar.config.js 	modified:   Rozzcov/quasar-project/src/App.vue 	new file:   Rozzcov/quasar-project/src/assets/Group 1.png 	new file:   Rozzcov/quasar-project/src/assets/Group 1.svg 	new file:   Rozzcov/quasar-project/src/assets/Group 2.png 	deleted:    Rozzcov/quasar-project/src/assets/quasar-logo-vertical.svg 	new file:   Rozzcov/quasar-project/src/components/app-kurs.vue 	modified:   Rozzcov/quasar-project/src/css/app.scss 	new file:   Rozzcov/quasar-project/src/css/reset.scss 	deleted:    Rozzcov/quasar-project/src/layouts/MainLayout.vue 	deleted:    Rozzcov/quasar-project/src/pages/ErrorNotFound.vue deleted:    Rozzcov/quasar-project/src/pages/IndexPage.vue 	modified:   Rozzcov/quasar-project/src/router/index.js

---
+++ Rozzcov/quasar-project/src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import AppKurs from '../src/components/app-kurs.vue';
+export default {
+  components:{
+    AppKurs,
+  },
+  data(){
+    return{
+
+    };
+  }
+
+}
+
+</script>
+
+<template>
+ <app-kurs></app-kurs>
+</template>  
---
+++ Rozzcov/quasar-project/src/components/app-kurs.vue
@@ -0,0 +1,110 @@
+<script>
+export default {
+
+  data(){
+    return{
+
+       cards: [
+         {title:"ОЛИМПИАДЫ ПО ИСТОРИИ",textes: ["Отечественная история","Мировая история","Культура"],color:["#7800FF"]},
+         {title:"ОЛИМПИАДЫ ПО МЕЖДУНАРОДНЫМ ОТНОШЕНИЯМ",textes: ["Теория международных отношений","Международные организации"],color:["#4800FF"]},
+         {title:"ОЛИМПИАДЫ ПО ПОЛИТОЛОГИИ", textes: ["Политология","Политический анализ","Философия"],color:["#3548FF"]},
+         {title:"ОЛИМПИАДЫ ПО СОЦИОЛОГИИ", textes: ["В процессе"],color:"#FFFFFF"},
+
+       ],
+    
+    };
+  }
+
+}
+
+</script>
+
+<template>
+  <div class="row">
+      <div class="menu col-2">
+        menu
+      </div>
+      <div class="col">
+         <div class="container">
+        <div class="row content">
+         <div class="col-12 tittle">
+            <span>КРУРСЫ</span>
+         </div>
+         <div class="col-12 search">
+
+
+           <q-input   class='search-input'  rounded outlined v-model="text" label="Нажмите чтобы начать поиск" />
+
+           
+         </div>
+
+
+
+            <div v-for="card in cards" :key="card.id" class="col-4 card"  :style="{background:card.color}" >
+               <div class="card-tittle">
+                  {{card.title}}
+               </div>
+               <div class="card-text">
+               <ul  class="card-list">
+                  <li v-for="(card,index) in cards" :key="index.id"  >{{ card.textes[index] }}</li>
+               </ul>
+            </div>
+            </div>
+    
+         
+         </div>
+        </div>
+      </div>
+      </div>
+    
+ </template>  
+
+
+<style scoped>
+
+.search-input {
+   height: 30px;
+   margin-bottom: 60px;
+}
+
+.tittle{
+   margin-bottom: 35px;
+}
+
+.card-tittle > li{
+    
+}
+.card{
+   width: 712px;
+   height: 370px;
+   border-radius: 5%;
+   padding: 48px;
+   margin: 20px;
+   outline: 1px solid black;
+}
+
+.card{
+   color:white;
+}
+.card:last-child{
+   color:#737373;
+}
+.card-tittle{
+   font-size: 50px;
+   line-height: 140%;
+}
+.card-list > li{
+   font-size: 20px;
+line-height: 250%;
+}
+.card-list > li::after{
+   content: ' ';
+   position: relative;
+   left: -5;
+   width: 5px;
+   height: 5px;
+   background: white;
+   border-radius: 50%;
+}
+
+</style>
