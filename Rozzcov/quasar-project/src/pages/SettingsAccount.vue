06a1ba34bddacde4376b05e818816941a942567b
Помазкин Николай <koliapomazkin@yandex.ru>
2024-07-11 17:06:14 +0700

    modified:   Rozzcov/quasar-project/src/pages/SettingsAccount.vue

--- Rozzcov/quasar-project/src/pages/SettingsAccount.vue
+++ Rozzcov/quasar-project/src/pages/SettingsAccount.vue
@@ -6,11 +6,9 @@
     <div class="title">
         НАСТРОЙКИ
     </div>
-    <div class="content">
-        <div class="row">
-
-            <div class="col-6">
-                <q-card class="card card-data">
+    <div class="row content">
+ <div class="col-6 column">
+    <div class="col-5"><q-card class="card card-data">
                     <div class="icon_block">
                         <img class="icon_person" src="src/assets/icons/person.svg">
                         <span class="data-text">Личные данные</span>
@@ -20,74 +18,27 @@
                         <div class="row">
                             <div class="col family_block">
                                 <label>Фамилия</label>
-                                <q-input rounded outlined class="input input_family"></q-input>
+                                <q-input dense rounded outlined class="input input_family"></q-input>
                             </div>
                             <div class="col name_block">
                                 <label>Имя</label>
-                                <q-input rounded outlined class="input input_name input-sm"></q-input>
+                                <q-input dense rounded outlined class="input input_name input-sm"></q-input>
                             </div>
                         </div>
                         <div class="row">
                             <div class="col secondfam_block">
                                 <label>Отчество</label>
-                                <q-input rounded outlined class="input input_second input-sm"></q-input>
+                                <q-input dense rounded outlined class="input input_second input-sm"></q-input>
                             </div>
                             <div class="col secondfam_block birthday_block ">
                                 <label>Дата рождения</label>
-                                <q-input class="input_date" outlined square v-model="text" hint="" :dense="dense"
+                                <q-input  class="input_date" outlined square v-model="text" hint="" :dense="dense"
                                     type="date" />
                             </div>
                         </div>
                     </div>
-                </q-card>
-            </div>
-            <!-- Управление подпиской -->
-            <div class="col-6">
-                <q-card class="card card_control">
-                    <div class="icon_block">
-                        <img class="icon_person" src="src/assets/icons/rouble.svg">
-                        <span class="data-text">Управление подпиской</span>
-                    </div>
-                    <div class="statuses">
-                        <div class="actual">
-                            <span>Текущий статус:</span>
-                            <p class="statuses_text">FREE</p>
-                        </div>
-                        <div class="next">
-                            <span>Следующее списание:</span>
-                            <p class="statuses_text">-</p>
-                        </div>
-                        <div class="choice_plan">
-                            <span>Выбор плана</span>
-                            <div class="subscribe">
-                                <div class="free sub_block">
-                                    <p class="sub_title">FREE</p>
-                                    <p class="sub_text">Доступ к бесплатному материалу</p>
-                                    <p class="price">0 руб./мес.</p>
-                                    <button class="plan_btn free">Текущий план</button>
-                                </div>
-                                <div class="base sub_block">
-                                    <p class="sub_title">BASE</p>
-                                    <p class="sub_text">Доступ ко всему материалу</p>
-                                    <p class="price price_base">500 руб./мес.</p>
-                                    <button class="plan_btn free">Оформить</button>
-                                </div>
-                                <div class="pro sub_block">
-                                    <p class="sub_title">PRO</p>
-                                    <p class="sub_text">Доступ ко всему материалу и наставничеству</p>
-                                    <p class="price price_pro">5000 руб./мес.</p>
-                                    <p class="pro_tutorship_text">подробнее про наставничество тут</p>
-                                    <button class="plan_btn free">Оформить</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </q-card>
-            </div>
-
-
-            <div class="col-5">
-                <q-card class="card card-security">
+                </q-card></div>
+    <div class="col-6"><q-card class="card card-security">
                     <div class="icon_block">
                         <img class="icon_person" src="src/assets/icons/key.svg">
                         <span class="data-text security-text">Вход и безопасность</span>
@@ -98,7 +49,7 @@
 
                                 <div class="row fit row wrap justify-center items-center content-center posta_grid">
                                     <label class="posta_label col-12">Почта</label>
-                                    <q-input rounded outlined class="input input_posta input-sm col-8"
+                                    <q-input dense rounded outlined class="input input_posta input-sm col-8"
                                         type="email"></q-input>
                                     <div class="change-btn-block col-4">
                                         <q-btn class="change_btn">Изменить</q-btn>
@@ -120,7 +71,7 @@
 
                                 <div class="row fit row wrap justify-center items-center content-center password_grid">
                                     <label class="password_label col-12">Пароль</label>
-                                    <q-input rounded outlined class="input input_password  col-8"
+                                    <q-input dense rounded outlined class="input input_password  col-8"
                                         type="password"></q-input>
                                     <div class="change-btn-block col-4">
                                         <q-btn class="change_btn">Изменить</q-btn>
@@ -138,7 +89,7 @@
 
                                 <div class="row fit row wrap justify-center items-center content-center card_grid">
                                     <label class="card_label col-12">Привязанная карта</label>
-                                    <q-input rounded outlined class="input input_card  col-8" type="text"></q-input>
+                                    <q-input dense rounded outlined class="input input_card  col-8" type="text"></q-input>
                                     <div class="change-btn-block col-4">
                                         <q-btn class="change_btn">Изменить</q-btn>
                                     </div>
@@ -153,9 +104,61 @@
 
                     </div>
                 </q-card>
-            </div>
+</div>
+ </div>
+<div class="col-6"><q-card class="card card_control">
+                  <div class="icon_block">
+                      <img class="icon_person" src="src/assets/icons/rouble.svg">
+                      <span class="data-text">Управление подпиской</span>
+                  </div>
+                  <div class="statuses">
+                      <div class="actual">
+                          <span>Текущий статус:</span>
+                          <p class="statuses_text">FREE</p>
+                      </div>
+                      <div class="next">
+                          <span>Следующее списание:</span>
+                          <p class="statuses_text">-</p>
+                      </div>
+                      <div class="choice_plan">
+                          <span>Выбор плана</span>
+                          <div class="subscribe">
+                              <div class="free sub_block">
+                                  <p class="sub_title">FREE</p>
+                                  <p class="sub_text">Доступ к бесплатному материалу</p>
+                                  <p class="price">0 руб./мес.</p>
+                                  <button class="plan_btn free">Текущий план</button>
+                              </div>
+                              <div class="base sub_block">
+                                  <p class="sub_title">BASE</p>
+                                  <p class="sub_text">Доступ ко всему материалу</p>
+                                  <p class="price price_base">500 руб./мес.</p>
+                                  <button class="plan_btn free">Оформить</button>
+                              </div>
+                              <div class="pro sub_block">
+                                  <p class="sub_title">PRO</p>
+                                  <p class="sub_text">Доступ ко всему материалу и наставничеству</p>
+                                  <p class="price price_pro">5000 руб./мес.</p>
+                                  <p class="pro_tutorship_text">подробнее про наставничество тут</p>
+                                  <button class="plan_btn free">Оформить</button>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </q-card>
+
+</div>
+              
+
+
+          
+
+     
         </div>
-    </div>
+
+     
+
+
 </template>
 
 <style>
@@ -197,7 +200,7 @@
 }
 
 .card {
-    margin: 4vh;
+    
     padding-top: 15px;
     padding-left: 35px;
     border: 1px solid black;
@@ -215,7 +218,8 @@
 }
 
 .card-security {
-    height: 530px;
+    height: 480px;
+    margin-top: 20px;
 }
 
 .input {
@@ -280,7 +284,7 @@
 }
 
 .card_control {
-    height: 820px;
+    height: 857px;
 }
 
 .statuses {
